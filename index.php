604800e05b17bb3144eb659509daa89403202a98
Uriel <uriellopezaquino1.0@gmail.com>
2021-07-16 15:12:19 -0500

    creando formulario

--- index.php
+++ index.php
@@ -4,4 +4,29 @@
     <h1>  Agenda de contactos </h1> 
 </div>
 
+<div class="bg-amarillo contenedor sombra">
+    <form id="contacto" action="#">
+        <legend>Añada un contacto<span>Todos los campos son abligatorios</span>
+        </legend>
+
+        <div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="nombre contacto" id="nombre">
+            </div>
+            <div class="campo">
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="nombre Empresa" id="empresa">
+            </div>
+            <div class="campo">
+                <label for="telefono">Telefono:</label>
+                <input type="tel" placeholder="nombre contacto" id="telefono">
+            </div>
+            <div class="campo enviar">
+                <input type="submit" value="añadir">
+            </div>
+        </div>
+    </form>
+</div>
+
 <?php include 'inc/layout/footer.php'; ?>
