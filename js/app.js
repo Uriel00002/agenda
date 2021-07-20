6bef4bf4d23a394da178c8ad090ec2f02f759e16
Uriel <uriellopezaquino1.0@gmail.com>
2021-07-19 19:18:21 -0500

    crear notificacion de vacio

--- js/app.js
+++ js/app.js
@@ -13,4 +13,34 @@ function leerFormulario(e) {
     const nombre = document.querySelector('#nombre').value;
     const empresa = document.querySelector('#empresa').value;
     const telefono = document.querySelector('#telefono').value;
+
+    if(nombre === '' || empresa === '' || telefono === ''){
+        mostrarNotificacion('Todos los campos son obligatorios', 'error');
+        
+    }
+    else {
+
+    }
+}
+
+
+function mostrarNotificacion(mensaje, clase) {
+    const notificacion = document.createElement('div');
+    notificacion.classList.add(clase, 'notificacion', 'sombra');
+    notificacion.textContent = mensaje;
+
+    //formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector('form leyend'));
+
+
+    //ocultary mostrar notificacion
+    setTimeout (() => {
+        notificacion.classList.add('visible');
+        setTimeout(() => {
+            notificacion.classList.remove('visible');
+            setTimeout(() =>{
+                notificacion.remove();
+            }, 500)
+        }, 3000)
+    }, 100);
 }
